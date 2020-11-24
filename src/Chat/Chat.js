689472c7a5a8b5d90f7e2b962917b8a6f4008441
yuming chen <mathcym@outlook.com>
2020-11-25 00:39:52 +0800

    [yuming chen]: finish problem 1

--- src/Chat/Chat.js
+++ src/Chat/Chat.js
@@ -5,6 +5,7 @@ import ChatBox from './ChatBox/ChatBox';
 import ChatInput from './ChatInput/ChatInput';
 import shopData from '../data/shop.json';
 import answersData from '../data/answers.json';
+import { ROLE } from '../constants';
 
 class Chat extends Component {
   constructor(props, context) {
@@ -12,6 +13,7 @@ class Chat extends Component {
     this.state = {
       shop: {},
       messages: [],
+      inputText: '',
     };
   }
 
@@ -27,13 +29,50 @@ class Chat extends Component {
     }, 1000);
   }
 
+  handleInputText = (event) => {
+    this.setState({
+      inputText: event.target.value,
+    });
+  };
+
+  handleSendMessage = () => {
+    const currentMessage = this.state.inputText;
+    const message = { role: ROLE.CUSTOMER, text: currentMessage };
+    let allMessgaes = this.state.messages.concat(message);
+    let preDefinedAnswer;
+    for (let i = 0; i < answersData.length; i += 1) {
+      const answer = answersData[i];
+      for (let j = 0; j < answer.tags.length; j += 1) {
+        if (currentMessage.includes(answer.tags[j])) {
+          preDefinedAnswer = answer;
+          break;
+        }
+      }
+    }
+    if (preDefinedAnswer !== undefined) {
+      allMessgaes = allMessgaes.concat(preDefinedAnswer);
+    }
+    this.setState({
+      inputText: '',
+    });
+    setTimeout(() => {
+      this.setState({
+        messages: allMessgaes,
+      });
+    }, 1000);
+  };
+
   render() {
-    const { shop, messages } = this.state;
+    const { shop, messages, inputText } = this.state;
     return (
       <main className="Chat">
         <ChatHeader shop={shop} />
         <ChatBox messages={messages} />
-        <ChatInput />
+        <ChatInput
+          text={inputText}
+          handleChatInput={this.handleInputText}
+          handleChat={this.handleSendMessage}
+        />
       </main>
     );
   }
